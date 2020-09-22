f3e6b1360a5075a25ce3b6fc029215740ecb3fb9
matt969696 <matsce2020@gmail.com>
2020-09-22 06:58:14 +0000

    Fixing solution 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,11 +6,16 @@
  */
 int main(void)
 {
-	char myString[] = "Holberton";
-	int i;
-
-	for (i = 0; i < 9; i++)
-		_putchar(myString[i]);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 	return (0);
+
 }
