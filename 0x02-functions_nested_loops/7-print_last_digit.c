90567e0c1d71916d8a40d32deab730f4f639ae59
matt969696 <matsce2020@gmail.com>
2020-09-22 07:27:31 +0000

    New push for 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,11 +10,11 @@
 
 int print_last_digit(int n)
 {
-	if (n > 0)
+	if (n < 0)
 	{
-		_putchar(n % 10 + '0');
-		return (n % 10);
+		_putchar(-n % 10 + '0');
+		return (-n % 10);
 	}
-	_putchar(-n % 10 + '0');
-	return (-n % 10);
+	_putchar(n % 10 + '0');
+	return (n % 10);
 }
