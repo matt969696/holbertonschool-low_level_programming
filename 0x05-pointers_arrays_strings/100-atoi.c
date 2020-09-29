7b2a037b3f45e5116f1d10dd385d736ed869676d
matt969696 <matsce2020@gmail.com>
2020-09-29 07:32:19 +0000

    Fixed Betty style for 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,12 +11,12 @@
 
 int _strlen(char *s)
 {
-        int i = 0;
+	int i = 0;
 
-        while (*(s + i) != '\0')
-                i++;
+	while (*(s + i) != '\0')
+		i++;
 
-        return (i);
+	return (i);
 }
 
 
@@ -30,26 +30,26 @@ int _strlen(char *s)
 
 int _atoi(char *s)
 {
-        int sign = 1;
-        int res = 0;
-        int i, len;
+	int sign = 1;
+	int res = 0;
+	int i, len;
 
-        i= 0;
-        len = _strlen(s);
+	i = 0;
+	len = _strlen(s);
 
-        while(i < len)
-        {
-                if (res == 0)
-                {
-                        if (*(s + i) == '-')
-                                sign = -sign;
-                }
+	while (i < len)
+	{
+		if (res == 0)
+		{
+			if (*(s + i) == '-')
+				sign = -sign;
+		}
 
-                if (*(s + i) >= '0' && *(s + i) <= '9')
-                        res = 10 * res + sign * (*(s + i) - '0');
-                else if (res != 0)
-                        break;
-                i++;
-        }
-        return (res);
+		if (*(s + i) >= '0' && *(s + i) <= '9')
+			res = 10 * res + sign * (*(s + i) - '0');
+		else if (res != 0)
+			break;
+		i++;
+	}
+	return (res);
 }
