f18d67597c71ed473d7e2eea8a7e1cc3c2ffd510
matt969696 <matsce2020@gmail.com>
2020-10-07 10:56:04 +0000

    fix(5) : simplified solution

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,26 +6,18 @@
  *
  * @n : number to be squared root
  * @low : lower test
- * @high : higher test
  *
  * Return: square root of n or -1
  */
 
-int sqrt_dich(int n, int low, int high)
+int sqrt_dich(int n, int low)
 {
 	if (low * low == n)
 		return (low);
-	if (high * high == n)
-		return (high);
-	if (high - low <= 1)
+	if (low * low > n)
 		return (-1);
-	if (((low + high) / 2 * (low + high) / 2) == n)
-		return ((low + high) / 2);
-	if (((low + high) / 2 * (low + high) / 2) > n)
-		return (sqrt_dich(n, low, (low + high) / 2));
-	return (sqrt_dich(n, (low + high) / 2, high));
+	return (sqrt_dich(n, low + 1));
 }
-
 
 
 /**
@@ -40,5 +32,5 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (sqrt_dich(n, 1, n / 2));
+	return (sqrt_dich(n, 0));
 }
