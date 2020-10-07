581743d9e4cf6c589b75f3d28e33426c9ff24e0d
matt969696 <matsce2020@gmail.com>
2020-10-07 11:52:03 +0000

    feat(100) : add initial solution

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+
+
+/**
+ * wildcmp - compares two strings
+ *
+ * @s1 : string 1
+ * @s2 : string 2 (can contain *)
+ *
+ * Return: 1 if strings are identical, else 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == 0)
+	{
+		if (*s2 == 0)
+			return (1);
+		if (*s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+		}
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
+	{
+		if (wildcmp(s1 + 1, s2) == 1 || wildcmp(s1, s2 + 1) == 1)
+			return (1);
+	}
+	return (0);
+}
