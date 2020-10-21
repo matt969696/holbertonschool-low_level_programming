81e3f83c73422469659245156b92402be446ea5c
matt969696 <matsce2020@gmail.com>
2020-10-21 10:23:11 +0000

    fix(2) : add NULL conditions

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,6 @@
 #include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * int_index - search for an integer
@@ -14,6 +16,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 	int ret = -1;
+
+	if (array == NULL || cmp == NULL)
+		exit(1);
 
 	while (i < size && ret == -1)
 	{
