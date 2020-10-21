fd83f6bb750114c26b83a40a851bf70d6ec535d2
matt969696 <matsce2020@gmail.com>
2020-10-21 10:18:57 +0000

    fix(1) : add NULL conditions

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,6 @@
 #include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * array_iterator - execute function on each element of
@@ -14,7 +16,8 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
-
+	if (array == NULL || action == NULL)
+		exit(1);
 	for (i = 0; i < size; i++)
 		action(array[i]);
 
