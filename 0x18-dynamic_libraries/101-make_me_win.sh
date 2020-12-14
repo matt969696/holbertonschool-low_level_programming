f74dc36da27efd3ec927de1c2f40cdd98df323d0
matt969696 <matsce2020@gmail.com>
2020-12-14 14:27:37 +0000

    feat() : initial solution 101

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -O /tmp/unrandom.so https://github.com/matt969696/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/unrandom.so
+export LD_PRELOAD=/tmp/unrandom.so
