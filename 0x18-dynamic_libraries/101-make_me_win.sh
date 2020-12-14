4bfa78ba55681821bd7452b0fc7451072872ba57
matt969696 <matsce2020@gmail.com>
2020-12-14 14:43:00 +0000

    fix() : correct bug

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/unrandom.so https://github.com/matt969696/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/unrandom.so
+wget -O /tmp/unrandom.so https://github.com/matt969696/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/unrandom.so
 export LD_PRELOAD=/tmp/unrandom.so
