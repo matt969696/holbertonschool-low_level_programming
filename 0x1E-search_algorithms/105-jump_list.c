9c503b0f3e47674900281aa8579164f9c3cc68ac
matt969696 <matsce2020@gmail.com>
2021-04-20 11:25:24 +0000

    fix() : correct second search

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -31,7 +31,7 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 			if (tail->next != NULL)
 				tail = tail->next;
 		}
-		printf("Value checked array[%lu] = [%i]\n", tail->index, tail->n);
+		printf("Value checked at index[%lu] = [%i]\n", tail->index, tail->n);
 		if ((i + step) >= size || tail->n > value)
 		{
 			printf("Value found between indexes [%lu] and [%lu]\n",
